5d7fe0dfba11bf13a989c55bd72c2087b20579ed
ToddYang <yanggt97@gmail.com>
2018-02-23 13:08:05 -0500

    working text
    just a hello text o/p

--- capture_image.c
+++ capture_image.c
@@ -1,10 +1,9 @@
-#include "address_map_arm.h"
-#include "altera_up_avalon_character_buffer.h"
-#include "time.h"
-
+// #include "address_map_arm.h"
+//clean !
 #define KEY_BASE              0xFF200050
 #define VIDEO_IN_BASE         0xFF203060
 #define FPGA_ONCHIP_BASE      0xC8000000
+#define TEXT				  0xC9000000
 
 /* This program demonstrates the use of the D5M camera with the DE1-SoC Board
  * It performs the following: 
@@ -18,33 +17,32 @@ int main(void)
 	volatile int * KEY_ptr				= (int *) KEY_BASE;
 	volatile int * Video_In_DMA_ptr	= (int *) VIDEO_IN_BASE;
 	volatile short * Video_Mem_ptr	= (short *) FPGA_ONCHIP_BASE;
-	/*
-	* initializing the alt up char buffer to writ stuff
-	*/
-	char* name = (char*) "DE1_SoC_With_D5M"		//name for the device
-	char* buffer;
-	alt_up_char_buffer_init(alt_up_char_buffer_dev name));
-	char message = 0xff;
-	
-	/*
-	* time
-	*/
-	time_t rawtimel
-	struct tm * timeinfo;
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);			
-	
+	volatile short * TXT	= (short *) TEXT;
+	int offset;
+	char *text_ptr;
+
 	int x, y;
 
 	*(Video_In_DMA_ptr + 3)	= 0x4;				// Enable the video
 
+	text_ptr = "hello";
+	printf("%s\n",text_ptr);
+	offset = (0 << 7) + 1;
+	while ( *(text_ptr) )
+	{
+		*(TXT + offset) = *(text_ptr); // write to the character buffer
+		printf("%s",*(TXT + offset));
+		++text_ptr;
+		++offset;
+	}
+	
+	
+	
 	while (1)
 	{
 		if (*KEY_ptr != 0)						// check if any KEY was pressed
 		{
 			*(Video_In_DMA_ptr + 3) = 0x0;			// Disable the video to capture one frame
-			alt_up_char_buffer_string(buffer, message, 10, 100); //draw random message 
-			printf("current time: \n", asctime(timeinfo);		//print time
 			while (*KEY_ptr != 0);				// wait for pushbutton KEY release
 			break;
 		}
@@ -61,8 +59,6 @@ int main(void)
 	for (y = 0; y < 240; y++) {
 		for (x = 0; x < 320; x++) {
 			short temp2 = *(Video_Mem_ptr + (y << 9) + x);
-						printf("current time: \n", asctime(timeinfo);		//print time
- 
 			*(Video_Mem_ptr + (y << 9) + x) = temp2;
 		}
 	}
